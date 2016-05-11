90bed26358ba00e538753bd85d4e0a39bad80c2c
NefariusVSP <g.o.m.e.z@yandex.ru>
2016-05-11 19:22:13 +0300

    Создание класса для заполнения бд таблицами улиц

---
+++ src/main/java/CreateBD.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+
+
+public class CreateBD {
+    // служит для заполнения бд таблицами улиц, принемает путь к файлу txt, и префикс района
+    static public void CreateStritTable (String path, String preficsNameTable) throws IOException {
+        String zap1 = "CREATE TABLE `spb`.`";
+        String zap2 = "` (\n" +
+                "  `home` VARCHAR(4) NOT NULL,\n" +
+                "  `housing` VARCHAR(4) NOT NULL,\n" +
+                "  `flat` VARCHAR(4) NOT NULL,\n" +
+                "  `lastName` VARCHAR(20) NOT NULL,\n" +
+                "  `fistName` VARCHAR(20) NOT NULL,\n" +
+                "  `midName` VARCHAR(20) NOT NULL,\n" +
+                "  `serialPassport` VARCHAR(4) NOT NULL,\n" +
+                "  `namberPassport` VARCHAR(6) NOT NULL,\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  PRIMARY KEY (`id`));\n";
+
+        try (Connection connection = DriverManager.getConnection(Programm.URL,Programm.USERNAME,Programm.PUSSWORD); Statement statement = connection.createStatement()) {
+
+            List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+            System.err.println(connection.isClosed());
+            int i = 1;
+            for (String line : lines) {
+                statement.execute(zap1 + preficsNameTable + i + zap2);
+                i += 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Programm.java
+++ src/main/java/Programm.java
@@ -1,9 +1,29 @@
-/**
- * Created by Sergei on 09.05.2016.
- */
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
+
 public class Programm {
-    static public void main(String[] arg){
-        System.err.println("HelloMaven");
-        
+
+    public static final String URL="jdbc:mysql://localhost:3306/spb";
+    public static final String USERNAME="root";
+    public static final String PUSSWORD=".12345qwert.";
+
+    static public void main(String[] arg) throws Exception{
+        CreateBD.CreateStritTable("\\\\LLL\\Users\\Public\\Documents\\1.txt","nev");
+        try{
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        try (Connection connection = DriverManager.getConnection(URL,USERNAME,PUSSWORD); Statement statement = connection.createStatement()){
+
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
